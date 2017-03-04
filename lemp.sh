60958fd63c12b0709364f7c49f46aade81b96b2b
maidonghu <maidonghu@users.noreply.github.com>
2017-03-04 09:05:12 -0800

    Update lemp.sh

--- lemp.sh
+++ lemp.sh
@@ -6,9 +6,11 @@ sudo apt-get install nginx -y
 #read -s -p "Enter MySQL Root Password: "  pswd
 #echo "mysql-server mysql-server/$pswd password root" | sudo debconf-set-selections
 #echo "mysql-server mysql-server/$pswd password root" | sudo debconf-set-selections
+export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get install -y -q mysql-server
 sudo mysql -sfu root < "mysql_secure_installation.sql"
+rm -f mysql_secure_installation.sql
 
-sudo apt-get install php-fpm php-mysql
+sudo apt-get install -y php-fpm php-mysql
 sudo sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.0/fpm/php.ini
 sudo systemctl restart php7.0-fpm
