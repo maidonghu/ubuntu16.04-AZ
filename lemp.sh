bc8e2c60290431ae8b6131f954ec9de572b9492f
maidonghu <maidonghu@users.noreply.github.com>
2017-03-07 12:37:03 -0800

    Update lemp.sh

--- lemp.sh
+++ lemp.sh
@@ -20,3 +20,4 @@ sudo sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.0/fpm/php.ini
 sudo systemctl restart php7.0-fpm
 
 sudo wget -O /etc/nginx/sites-available/default https://raw.githubusercontent.com/maidonghu/ubuntu16.04-AZ/master/nginx.conf
+sudo systemctl restart nginx
